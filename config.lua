db51979252ce13b17e7aa3705b9d297f8b98cc6f
lucaswang <118956159+lucaswang977@users.noreply.github.com>
2022-12-30 16:02:20 +0900

    Add fugitive plugin

--- config.lua
+++ config.lua
@@ -175,6 +175,25 @@ lvim.plugins = {
       require("spectre").setup()
     end,
   },
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "G",
+      "Git",
+      "Gdiffsplit",
+      "Gread",
+      "Gwrite",
+      "Ggrep",
+      "GMove",
+      "GDelete",
+      "GBrowse",
+      "GRemove",
+      "GRename",
+      "Glgrep",
+      "Gedit"
+    },
+    ft = { "fugitive" }
+  },
 }
 
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
